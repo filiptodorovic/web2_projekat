5eb4d3c6373af0aaac27c317184a977e926411c1
filiptodorovic <todorovic.pr25.2019@uns.ac.rs>
2023-08-09 22:41:28 +0200

    Testing database

--- backend/backendShop/backendShop/Controllers/WeatherForecastController.cs
+++ backend/backendShop/backendShop/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using backendShop.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,8 @@ namespace backendShop.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        private readonly DataContext _context;
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -18,15 +21,39 @@ namespace backendShop.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(DataContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
+
+            Models.User u = new Models.User();
+            u.Email = "nekiemail2@gmail.com";
+            u.Username = "username2";
+            u.Password = "password1";
+            u.Name = "Name1";
+            u.LastName = "LastName1";
+            u.DateOfBith = DateTime.Now;
+            u.Address = "Address1";
+            u.UserType = Models.UserType.BUYER;
+            u.PictureUrl = "SomeUrl.com";
+
+            _context.Users.Add(u);
+            _context.SaveChanges();
+
+            var user = _context.Users.Find("nekiemail@gmail.com");
+
+            if (((Models.User)user).Buyer != null) {
+                if (2 > 1) { 
+                
+                }
+            }
+
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
